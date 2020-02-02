c67ad6b73df18588608eda85ffbe056736c5c76f
Bernadette Davis <bsd231@nyu.edu>
2020-02-02 22:25:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here
+
+class Breakfast {
+  constructor(food, drink) {
+    this._ = ;
+    this. = ;
+  }
+}
+
+class Lunch {
+  constructor(food, drink) {
+    this._ = ;
+    this. = ;
+  }
+}
+
+class Dinner {
+  constructor(food, drink) {
+    this._ = ;
+    this. = ;
+  }
+}
